7e0e1f89d797cc3cc13518718c3db7f9bb849493
Egar19 <madeeegar@gmail.com>
2026-06-01 16:52:03 +0800

    change default locale to "id"

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-  defaultLocale: 'en',
+  defaultLocale: 'id',
   locales: ['en', 'id'],
 });
 
